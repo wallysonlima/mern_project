f4685f10f03d5936ab5354c2c940badd641b4f9f
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-06-30 09:50:49 -0300

    Test single react app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import react from 'react';
+import { render } from 'react-dom';
 
-const App {} => {
-    return <h1>A React App!</h1>;
-};
+//const App {} => {
+    //return <h1>A React App!</h1>;
+//    return React.createElement('h1', {}, 'Hi, this is React!');
+//};
+
+class App extends React.Component {
+    render() {
+        return <div>
+            <h3></h3>
+            <ul className="goal_list">
+                <li>Finish the Course</li>
+                <li>Lean all about the Course Main Topic</li>
+                <li>Jelp other students in the Course</li>
+            </ul>
+        </div>
+    }
+}
 
 export default App;
