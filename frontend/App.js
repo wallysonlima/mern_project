a5595cb7ea6543fd6af63edeb1a682f5d9c56a46
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-06-30 15:35:36 -0300

    finished by the day

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
 import Users from './user/pages/Users.js'
+import NewPlace from './places/pages/NewPlace'
 
 const App = () => {
     return <Router>
-        <Route path="/">
-
+        <Switch>
+        <Route path="/" exact>
+            <Users />
         </Route>
+        <Route path="/places/new" exact>
+            <NewPlace />
+        </Route>
+        <Redirect to="/">
+        </Switch>
     </Router>
 };
 
