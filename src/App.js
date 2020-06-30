c55784e895174b8444667601c5ae2016882ec940
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-06-30 09:58:14 -0300

    Created first react component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import react from 'react';
 import { render } from 'react-dom';
-
+import GoalList from './components/GoalList'
 //const App {} => {
     //return <h1>A React App!</h1>;
 //    return React.createElement('h1', {}, 'Hi, this is React!');
@@ -9,12 +9,8 @@ import { render } from 'react-dom';
 class App extends React.Component {
     render() {
         return <div>
-            <h3></h3>
-            <ul className="goal_list">
-                <li>Finish the Course</li>
-                <li>Lean all about the Course Main Topic</li>
-                <li>Jelp other students in the Course</li>
-            </ul>
+            <h3>Course Goals</h3>
+            <GoalList />
         </div>
     }
 }
