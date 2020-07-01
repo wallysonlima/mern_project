9e00bf6b081da12e35a224dd741d3919392d7de6
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-01 14:05:20 -0300

    Updated mainNavigation

--- frontend/shared/Navigation/SideDrawer.js
+++ frontend/shared/Navigation/SideDrawer.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {CSSTransition} from 'react-transition-group'
 
-impor './SideDrawer.css';
+import './SideDrawer.css';
 
 const SideDrawer = props => {
+    <CSSTransition 
+        in={props.show}
+        timeout={200} 
+        classNames="slide-in-left"
+        mountOnEnter
+        unmountOnExit
+    >
+        <aside className="side-drawer">{props.children}</aside>;
+    </CSSTransition>
 
+    return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
 };
 
 export default SideDrawer;
