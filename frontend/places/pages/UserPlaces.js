0c5614ff05f9d306eacf438a853710ac34251b35
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-01 14:46:01 -0300

    Finished by the day

---
+++ frontend/places/pages/UserPlaces.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES =  [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous building in the world',
+        imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fquantocustaviajar.com%2Fblog%2Fempire-state-building-em-nova-york%2F&psig=AOvVaw0PmKil5fYvbSQix7dBac1y&ust=1593711777272000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOC4o5bNrOoCFQAAAAAdAAAAABAD'
+        address: ' 20 w 34 th St, New York, NY 10001',
+        location:  
+    }
+]
+
+const UserPlaces = props => {
+    if (proprs.items.length === 0) {
+        return <div className="place-list center">
+            <Card>
+                <h2>No places found. Maybe create one?</h2>
+                <button>Share Place</button>
+            </Card>
+        </div>
+    }
+};
+
+export default UserPlaces;
