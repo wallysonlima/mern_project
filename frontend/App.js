3bf8be78a36d6a670fe9a89f3b840fefa03965b5
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-02 10:08:03 -0300

    Updated map.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
-import Users from './user/pages/Users.js'
-import NewPlace from './places/pages/NewPlace'
+import Users from './user/pages/Users.js';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
     return <Router>
-        <Switch>
-        <Route path="/" exact>
-            <Users />
-        </Route>
-        <Route path="/places/new" exact>
-            <NewPlace />
-        </Route>
-        <Redirect to="/">
-        </Switch>
+        <MainNavigation />
+        <main>
+            <Switch>
+            <Route path="/" exact>
+                <Users />
+            </Route>
+            <Route path="/:userId/places" exact></Route>
+                <UserPlaces />
+            <Route path="/places/new" exact>
+                <NewPlace />
+            </Route>
+            <Redirect to="/">
+            </Switch>
+        </main>
     </Router>
 };
 
--- frontend/places/components/PlaceItem.js
+++ frontend/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Modal from '../../shared/components/UIElements/Modal';
 import './PlaceItem.css';
+import Map from '../../shared/components/UIElements/Map';
 
 const PlaceItem = props => {
     const [showMap, setShowMap] = useState(false);
@@ -21,7 +22,7 @@ const PlaceItem = props => {
             footer={<Button onClick={closeMapHandler}>Close</Button>}  
         >
             <div className="map_container">
-                <h2>The MAp !</h2>
+                <Map center={props.coordinates} zoom={16}/>
             </div>
         </Modal>
         <li className="place-item">
