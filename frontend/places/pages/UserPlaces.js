eb933556bb2b3f406c3c93d71939407d0f196cf6
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-02 07:47:02 -0300

    PlaceItem.css

--- frontend/places/pages/UserPlaces.js
+++ frontend/places/pages/UserPlaces.js
@@ -3,7 +3,7 @@ import PlaceList from '../components/PlaceList';
 
 const DUMMY_PLACES =  [
     {
-        id: 'p1',
+        id: 'p1',clear
         title: 'Empire State Building',
         description: 'One of the most famous building in the world',
         imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fquantocustaviajar.com%2Fblog%2Fempire-state-building-em-nova-york%2F&psig=AOvVaw0PmKil5fYvbSQix7dBac1y&ust=1593711777272000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOC4o5bNrOoCFQAAAAAdAAAAABAD'
