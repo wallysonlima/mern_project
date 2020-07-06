1cc7e73e582940b6af9a138636e86d4b2563fb27
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-06 08:16:45 -0300

    Updated Auth.js

--- frontend/user/pages/Auth.js
+++ frontend/user/pages/Auth.js
@@ -2,16 +2,30 @@ import React from 'react';
 
 import './Auth.css';
 import Card from '../../shared/components/UIElements/Card';
-
+import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH} from '../..shared/util/validators';
+import {useForm} from '../../shared/hooks/form-hook';
 
 const Auth = () => {
-    return <Card> className="authenticate"
-        <h2>Login Required</h2>
-        <hr />
-        <form>
+    return (
+        <Card> className="authenticate"
+            <h2>Login Required</h2>
+            <hr />
+            <form>
+                <Input 
+                    element="input"
+                    id="email"
+                    type="email"
+                    label="E-Mail"
+                    validators={[VALIDATOR_EMAIL()]}
+                    errorText="Please enter a valid email address."
+                    onChange
+                >
 
-        </form>
-    </Card>
+                </Input>
+            </form>
+        </Card>
+    );
 };
 
 export default Auth;
