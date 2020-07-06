2e643c3c10b65daac816e92a1d67fe6a7d0cbe18
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-06 08:10:23 -0300

    Updated Auth

--- frontend/user/pages/Auth.js
+++ frontend/user/pages/Auth.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 import './Auth.css';
+import Card from '../../shared/components/UIElements/Card';
+
 
 const Auth = () => {
-    return <h2>Auth component!</h2>
+    return <Card> className="authenticate"
+        <h2>Login Required</h2>
+        <hr />
+        <form>
+
+        </form>
+    </Card>
 };
 
 export default Auth;
