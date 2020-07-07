8b2625b60104a086f47c00d581b7c255b99210fa
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-07 09:38:35 -0300

    using example with express

--- example/app.js
+++ example/app.js
@@ -1,25 +1,32 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log('INCOMING REQUEST');
-    console.log(req.method, req.url);
-    
-    if (req.method === 'POST') {
-        let body = '';
-        req.on('end', () => {
-            const userName = body.split('=')[1];
-            res.end('<h1>' + userName + '</h1>');
-        });
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    let body = '';
+    req.on('end', () => {
+        const userName = body.split('=')[1];
+        if (userName) {
+            req.body = { name:userName };
+        }
         
-        req.on('data', (chunk) => {
-            body += chunk;
-        });
-    } else {
-        res.setHeader('Content-Type', 'text/html');
-        res.end(
-            '<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
-        );
-    }   
+        next();
+    });
+    
+    req.on('data', chunk => {
+       body += chunk;
+   });
 });
 
-server.listen(5000);
+app.use((req, res, next) => {
+    if (req.body) {
+        return res.send('<h1>' + req.body.name + '</h1>'); 
+    }
+    
+    res.send('<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>');
+});
+
+
+app.listen(5000);
