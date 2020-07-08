96a356ad03e8b1012bfb1132375194db047a9b59
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-08 09:26:53 -0300

    add some code

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,7 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-    res.json({message: 'It works'});
-    console.log('GET Request in Places');
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: "One of the most famous sky scrapers in the world",
+        location: {
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: '20 w 34 th St, New Yorkm NY 10001',
+        creator: 'u1'
+    }
+];
+
+router.get('/:pid', function(req, res, next) {
+    const placeId = req.params.pid; //{pid: 'p1'}
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId;
+    });
+    res.json({place});
 });
+
+module.exports = router;
