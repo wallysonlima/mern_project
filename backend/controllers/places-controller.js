40314e0ef67c208b54e703de7f668c4c52aadae9
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-09 08:09:26 -0300

    rearranged the code

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -1,3 +1,17 @@
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: "One of the most famous sky scrapers in the world",
+        location: {
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: '20 w 34 th St, New Yorkm NY 10001',
+        creator: 'u1'
+    }
+];
+
 const HttpError = require("../models/http-error");
 
 const getPlaceById = function(req, res, next){
@@ -12,4 +26,22 @@ const getPlaceById = function(req, res, next){
         
     res.json({ place }); // => { place } => { place:place}
     }
+}
+
+// function getPlaceById() {...}
+// const getPlaceById = function() {...}
+const getPlaceByUserId = function(req, res, next) {
+    const userId = req.params.uid;
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId;
+    });
+
+    if (!place) {
+      return next (
+          new HttpError('Could not find a place for the provided user id.')
+      );
+    }
+
+    res.json({place});
 }
