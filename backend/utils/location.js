76e9325927adac8d558e2a3314b8c62cef38da6e
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-10 08:38:22 -0300

    add axios package

---
+++ backend/utils/location.js
@@ -0,0 +1,8 @@
+const API_KEY = 'AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA';
+
+async function getCoordsForAddress(address) {
+    return {
+        lat: 40.07484474,
+        lng: -73.9871516
+    };
+}
