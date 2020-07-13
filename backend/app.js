323d350506b794e66666df1552438df3ea7bb1ef
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-13 09:08:26 -0300

    Created placeSchema

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -23,5 +24,12 @@ app.use(function(error, req, res, next) {
    res.status(error.code || 500);
    res.json({message: error.message || 'An unknown error occurred!'}); 
 });
+
+mongoose
+    .connect('mongodb+srv://wally:1234@cluster0.jziz6.mongodb.net/places?retryWrites=true&w=majority')
+    .then(() => {
+        app.listen(5000);
+    });
+    
 
 app.listen(5000);
