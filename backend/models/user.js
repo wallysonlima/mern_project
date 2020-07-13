c3ad841b17181139e9081b219b1cfb0c0d38c71c
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-13 10:16:15 -0300

    install mongoose-unique-validator

---
+++ backend/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: {type: String, required: true, unique: true },
+    password: {type: String, required: true, minlength: 6 },
+    image: { type: String, required: true },
+    places: { type: String, required: true }
+});
