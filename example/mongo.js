8c294020422ee5f496fe7d5b3651cd992e7d64ec
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-13 08:25:36 -0300

    add some more stuff

--- example/mongo.js
+++ example/mongo.js
@@ -39,3 +39,28 @@ const getProducts = async function(req, res, next) {
 
 exports.createProduct = createProduct;
 exports.getProducts = getProducts;
+
+const createProduct = async function (req, res, next ) {
+    const newProduct = {
+        name: req.body.name,
+        price: req.body.price
+    };
+
+    const client = new MongoClient(url);
+
+    try {
+        await client.connect();
+        const db = client.db();
+        const result = db.collection('products').insertOne(newProduct);
+    } catch (error) {
+        return res.json({message: 'Could not store data.'});
+    };
+
+    client.close();
+
+    res.json(newProduct);
+};
+
+const getProducts = async function (req, res, next) {
+    const client = new MongoClient(url);
+}
--- example/mongoose.js
+++ example/mongoose.js
@@ -2,11 +2,22 @@ const mongoose = require('mongoose');
 
 const Product = require('./models/product');
 
+mongoose.connnect('')
+.then(() => {
+    console.log('Connected to database!');
+}).catch(() => {
+    console.log('Connection failed!');
+});
+
+
+
 const createProduct = async function (req, res, next) => {
     const createdProduct = new Product({
         name: req.body.name,
         price: req.body.price
     });
+
+    const result = await createdProduct.save();
 };
 
 exports.createProduct = createProduct;
