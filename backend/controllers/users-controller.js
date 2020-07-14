c1ab79460af5b24f31288482d0dbdf318ef071c0
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-14 08:18:26 -0300

    updated users-controller

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -2,6 +2,7 @@ const uuid = require('uuid/v4');
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const User = require('../models/user');
 
 const DUMMY_USERS = [
     {
@@ -25,22 +26,47 @@ const signup = function(req, res, next) {
     
     const{ name, email, password } = req.body;
 
-    const hasUser = DUMMY_USERS.find(u => u.email === email);
+    let existingUser;
 
-    if (hasUser) {
-        throw new HttpError('Could not create user, email already exists.', 422);
+    try {
+        existingUser = User.findOne({ email: email });
+    } catch ( err ) {
+        const error = new HttpError(
+            'Signing up failed, please trye again later.',
+            500
+        );
+
+        return next(error);
+    } 
+
+    if (existingUser) {
+        const error = new HttpError(
+            'User exists already, please login instead.',
+            422
+        );
+        return next(error);
     }
 
-    const createdUser = {
-        id: uuid(),
+    const createdUser = new User({
         name,
         email,
-        password
-    };
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.personare.com.br%2Fo-que-significa-sonhar-com-leao-m48714&psig=AOvVaw345FEMWGfHC9DUVs4mvq52&ust=1594811606431000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPjX06zOzOoCFQAAAAAdAAAAABAD',
+        password,
+        places
+    });
 
-    DUMMY_USERS.push(createdUser); 
+    try {
+        await createdUser.save();
+    } catch ( err ) {
+        const error = new HttpError(
+            'Sign up failed, please try again.',
+            500
+        );
 
-    res.status(201).json({user: createdUser});
+        return next(error);
+    }
+
+    res.status(201).json({user: createdUser.toObject({getters: true})});
 };
 
 
