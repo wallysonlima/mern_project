076a1a09e84129d5c018efc9120ef628f2f8bd27
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-14 09:56:01 -0300

    Finished the database part

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -8,9 +8,30 @@ router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
-router.post('/', placesControllers.createPlace);
-
-router.patch('/:pid', placesControllers.updatePlace);
+router.post(
+    '/',
+    [
+      check('title')
+        .not()
+        .isEmpty(),
+      check('description').isLength({ min: 5 }),
+      check('address')
+        .not()
+        .isEmpty()
+    ],
+    placesControllers.createPlace
+  );
+  
+  router.patch(
+    '/:pid',
+    [
+      check('title')
+        .not()
+        .isEmpty(),
+      check('description').isLength({ min: 5 })
+    ],
+    placesControllers.updatePlace
+  );
 
 router.delete('/:pid', placesControllers.deletePlace);
 
