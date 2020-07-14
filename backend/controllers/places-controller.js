b952d0251ece73dda9e8246292f0c750174b675a
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-14 08:23:42 -0300

    fixed minor errors

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -106,7 +106,9 @@ const updatePlace = async function (req, res, next) {
     const errors = validationResult(req);
     
     if (!errors.isEmpty()) {
-        throw new HttpError('Invalid inputs passed, please check your data.', 422);
+       return next (
+            new HttpError('Invalid inputs passed, please check your data.', 422);
+       );
     }
     
     const { title, description } = req.body;
