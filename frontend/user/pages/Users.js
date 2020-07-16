13068bf35d0271e5909f866a38ec432e5445729d
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-16 09:56:57 -0300

    more stuff

--- frontend/user/pages/Users.js
+++ frontend/user/pages/Users.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import UsersList from '../components/UserList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+
+// Conecta os usuarios no backend
 const Users = () => {
-    const USERS = [
-        {
-            id: 'u1', 
-            name: "Wallyson Lima", 
-            image: 'https://www.google.com.br/url?sa=i&url=https%3A%2F%2Fveja.abril.com.br%2Fblog%2Fisabela-boscov%2Fo-rei-leao-2019%2F&psig=AOvVaw3tPIfB80R9uGqDW8vUy8t9&ust=1593687559981000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOiGqvryq-oCFQAAAAAdAAAAABAD', 
-            places: 3
-        }
-    ];
+    const [isLoading, setIsLoading] = useState(false);
+    const [data, setError] = useState(false);
+    const [loadedUsers, setLoadedUsers] = useState(false);
 
+    useEffect(() => {
+        const sendRequest = async () => {
+            setIsLoading(true);
+            
+            try {
+                const response = await fetch('http://localhost:5000/api/users');
+            
+                const responseData = await response.json();
+            
+                if (!response.ok) {
+                    throw new Error(responseData.message);
+                }
 
-    return <UsersList items={USERS} />;
+                setLoadedUsers(responseData.users);
+                
+            } catch (err) {
+                setError(err.message);
+            }
+          
+            setIsLoading(false);
+        };
+        sendRequest();
+    }, []);
+
+    const errorHandler = () => {
+        setError(null);
+    }
+
+    return 
+    <React.Fragment>
+        <ErrorModal error={error} onClear={errorHandler} />
+        {isLoading && (
+            <div className="center">
+                <LoadingSpinner />
+            </div>
+        )}
+        {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
 }
