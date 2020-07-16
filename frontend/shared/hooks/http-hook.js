9af01c6c1dccb9822a33c13e01657ff7f40c3f9c
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-16 10:30:10 -0300

    Created http-hook.js file

---
+++ frontend/shared/hooks/http-hook.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+export const useHttpClient = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+
+    const sendRequest = useCallBack(async (url, method = 'GET', body = null, headers = {}) => {
+        setIsLoading(true);
+        try {
+            const response = await fetch(url, {
+                method,
+                body,
+                headers
+            });
+    
+            const responseData = await response.json();
+                
+            if (!response.ok) {
+                throw new Error(responseData.message);
+            }
+
+            return responseData;
+        } catch(err) {
+            setError(err.message);
+        }
+        
+        setIsLoading(false);
+    }, []);
+
+    const clearError = => {
+        setError(null);
+    };
+
+    return { isLoading, error, sendRequest};
+};
