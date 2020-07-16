d700e5ab2f088581289bc58e416e46c7951698e5
wallysonlima <wallyson.n.a.lima@gmail.com>
2020-07-16 10:41:15 -0300

    Created http-hook.js file

--- frontend/shared/hooks/http-hook.js
+++ frontend/shared/hooks/http-hook.js
@@ -1,16 +1,23 @@
-import { useState } from 'react';
+import { useState, useCallback, useRef, useEffect } from 'react';
 
 export const useHttpClient = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState();
 
+    const activeHttpRequests = useRef([]);
+
     const sendRequest = useCallBack(async (url, method = 'GET', body = null, headers = {}) => {
         setIsLoading(true);
+        
+        const httpAbortCtrl = new AbortController();
+        activeHttpRequests.current.push(httpAbortCtrl);
+        
         try {
             const response = await fetch(url, {
                 method,
                 body,
-                headers
+                headers,
+                signal: httpAbortCtrl.signal
             });
     
             const responseData = await response.json();
@@ -27,9 +34,15 @@ export const useHttpClient = () => {
         setIsLoading(false);
     }, []);
 
-    const clearError = => {
+    const clearError = () => {
         setError(null);
     };
+
+    useEffect(() => {
+        return () => {
+            activeHttpRequests.current.forEach(abortCtrl => abortCtrl.abortCtrl());
+        };
+    }, []);
 
     return { isLoading, error, sendRequest};
 };
